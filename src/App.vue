f0381f891279650a332abb6255211d74e0c1eb2c
MoellerPaula <moeler.paula@gmail.com>
2026-07-23 11:17:11 +0200

    feat: add intitial page structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div>
+    <header>
+      <h1>CO₂-Footprint</h1>
+    </header>
+
+    <main>
+      <h2>CO₂-Emissionen</h2>
+      <p>
+        Transparenz über die jährlichen CO₂-Emissionen von Ländern und Unternehmen.
+      </p>
+    </main>
+
+    <footer>
+      <p>&copy; 2026 CO₂-Footprint</p>
+    </footer>
+  </div>
 </template>
