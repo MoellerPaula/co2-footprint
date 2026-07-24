6bfd670e8c77fb979bd671b4fa4dcd73027d413b
MoellerPaula <moeler.paula@gmail.com>
2026-07-24 23:04:12 +0200

    feat: add initial header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
 <template>
-  <div>
-    <header>
-      <h1>CO₂-Footprint</h1>
-    </header>
-
-    <main>
-      <h2>CO₂-Emissionen</h2>
-      <p>
-        Transparenz über die jährlichen CO₂-Emissionen von Ländern und Unternehmen.
-      </p>
-    </main>
-
-    <footer>
-      <p>&copy; 2026 CO₂-Footprint</p>
-    </footer>
-  </div>
+  <Header />
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="ps-5 pt-5">
+    <div class="d-flex align-items-center gap-5">
+      <img src="../assets/LogoCO2Fußabdruck3.svg" alt="Logo"/>
+      <h1>CO₂ Fußabdruck</h1>
+    </div>
+
+    <nav class="d-flex justify-content-end gap-5">
+      <a href="#" class="active">Startseite</a>
+      <a href="#">Emissionsdaten</a>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+img {
+  width: 4rem;
+  height: auto;
+}
+
+h1 {
+    color: #006b0eff;
+    font-weight: bold;
+}
+
+nav {
+    padding-right: 6rem;
+}
+
+a {
+    color: #006b0eff;
+    font-weight: bold;
+    font-size: 1.2rem;
+    text-decoration: none;
+}
+
+a.active {
+    border-bottom: 2px solid #006b0eff;
+}
+</style>
