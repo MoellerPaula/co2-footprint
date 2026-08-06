1cd94428ada727235735990329bd0a3a747078ca
MoellerPaula <moeler.paula@gmail.com>
2026-08-06 18:57:51 +0200

    feat: adapt layout direction based on browser language

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup>
+import { onMounted } from 'vue';
 import AppHeader from './components/AppHeader.vue';
 import AppFooter from './components/AppFooter.vue';
+
+onMounted(() => {
+  const rtlLanguages = ['ar', 'he', 'fa', 'ur'];
+  const lang = navigator.language.split('-')[0];
+  document.documentElement.dir = rtlLanguages.includes(lang) ? 'rtl' : 'ltr';
+});
 </script>
 
 <template>
